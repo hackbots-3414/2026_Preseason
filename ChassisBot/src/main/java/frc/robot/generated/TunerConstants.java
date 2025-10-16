66d62c7a377c73117de05ca703420502bc932987
Nolan Barker <107814443+TheRekrab@users.noreply.github.com>
2025-10-16 18:55:53 -0400

    We created tunner constants for chassis bot

--- ChassisBot/src/main/java/frc/robot/generated/TunerConstants.java
+++ ChassisBot/src/main/java/frc/robot/generated/TunerConstants.java
@@ -24,14 +24,14 @@ public class TunerConstants {
     // The steer motor uses any SwerveModule.SteerRequestType control request with the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
-        .withKP(100).withKI(0).withKD(0.5)
-        .withKS(0.1).withKV(3.1).withKA(0)
+        .withKP(0.7473825).withKI(0).withKD(0)
+        .withKS(0.05913566667).withKV(3.039475).withKA(0.09555725)
         .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
     private static final Slot0Configs driveGains = new Slot0Configs()
-        .withKP(0.1).withKI(0).withKD(0)
-        .withKS(0).withKV(0.124);
+        .withKP(0.0154954).withKI(0).withKD(0)
+        .withKS(0.21003).withKV(0.11971);
 
     // The closed-loop output type to use for the steer motors;
     // This affects the PID/FF gains for the steer motors
